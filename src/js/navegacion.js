043d7b35fa4561c7826248cb79a0c4776a4b27b1
kianlopez <233366@ids.upchiapas.edu.mx>
2025-11-14 08:24:53 -0600

    fakiuuuuuu karla

---
+++ src/js/navegacion.js
@@ -0,0 +1,39 @@
+// Archivo: navegacion.js
+document.addEventListener("DOMContentLoaded", () => {
+  const botones = document.querySelectorAll(".menu-btn");
+
+  botones.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const texto = btn.textContent.trim().toLowerCase();
+
+      switch (true) {
+        case texto.includes("animal"):
+          window.location.href = "animales.html";
+          break;
+
+        case texto.includes("medicamento"):
+          window.location.href = "medicamento.html";
+          break;
+
+        case texto.includes("tarjeta"):
+          window.location.href = "tarjetaSalud.html";
+          break;
+
+        case texto.includes("reporte"):
+          window.location.href = "reporteMedico.html";
+          break;
+
+        case texto.includes("tratamiento"):
+          window.location.href = "tratamiento.html";
+          break;
+
+        case texto.includes("enfermedad"):
+          window.location.href = "enfermedad.html";
+          break;
+
+        default:
+          console.warn("Botón no configurado:", texto);
+      }
+    });
+  });
+});
